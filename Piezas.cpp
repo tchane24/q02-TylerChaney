18b0e6c0babed4f09f946fd713b9910cd6a17d63
student <student@softeng>
2017-03-27 13:19:34 -0700

    My Implementation and Tests

--- Piezas.cpp
+++ Piezas.cpp
@@ -20,13 +20,27 @@
  * Constructor sets an empty board (default 3 rows, 4 columns) and 
  * specifies it is X's turn first
 **/
-Piezas::Piezas();
+Piezas::Piezas()
+{
+	board.resize(BOARD_ROWS, std::vector<Piece>(BOARD_COLS, Blank));
+	turn = O; // Setting turn to 0 because toggleTurn is called on each drop
+		// Therefore, when the first piece is dropped turn will be X
+};
 
 /**
  * Resets each board location to the Blank Piece value, with a board of the
  * same size as previously specified
 **/
-void Piezas::reset();
+void Piezas::reset()
+{
+	for(int i=0; i<BOARD_ROWS; i++)
+	{
+		for(int j=0; j<BOARD_COLS; j++)
+		{
+			board[i][j] = Blank;
+		}
+	}
+};
 
 /**
  * Places a piece of the current turn on the board, returns what
@@ -36,13 +50,43 @@ void Piezas::reset();
  * Out of bounds coordinates return the Piece Invalid value
  * Trying to drop a piece where it cannot be placed loses the player's turn
 **/ 
-Piece Piezas::dropPiece(int column);
+Piece Piezas::dropPiece(int column)
+{
+	if(turn == X)
+		turn = O;
+	else if(turn == O)
+		turn = X;
+	if(column > 3 || column < 0)
+		return Invalid;
+	if(board[0][column] == Blank)
+	{
+		board[0][column] = turn;
+		return turn;
+	}
+	else if(board[1][column] == Blank)
+	{
+		board[1][column] = turn;
+		return turn;
+	}
+	else if(board[2][column] == Blank)
+	{
+		board[2][column] = turn;
+		return turn;
+	}
+	return Blank;
+};
 
 /**
  * Returns what piece is at the provided coordinates, or Blank if there
  * are no pieces there, or Invalid if the coordinates are out of bounds
 **/
-Piece Piezas::pieceAt(int row, int column);
+Piece Piezas::pieceAt(int row, int column)
+{
+	if(row >= 0 && row < 3 && column >= 0 && column < 4)
+		return board[row][column];
+	// Should already return Blank if there is no piece there
+	return Invalid;
+};
 
 /**
  * Returns which Piece has won, if there is a winner, Invalid if the game
@@ -53,4 +97,87 @@ Piece Piezas::pieceAt(int row, int column);
  * or horizontally. If both X's and O's have the same max number of pieces in a
  * line, it is a tie.
 **/
-Piece Piezas::gameState();
+Piece Piezas::gameState()
+{
+	for(int i=0; i<BOARD_ROWS; i++)
+	{
+		for(int j=0; j<BOARD_COLS; j++)
+		{
+			if(board[i][j] == Blank)
+				return Invalid;
+		}
+	}
+	int xHigh = 0;
+	int xCur = 0;
+	int oHigh = 0;
+	int oCur = 0;
+	for(int i=0; i<BOARD_COLS; i++)
+	{
+		if(board[1][i] == X)
+		{
+			if(board[2][i] == X && board[0][i] == X)
+				xCur = 3;
+			else if(board[2][i] == X || board[0][i] == X)
+				xCur = 2;
+		}
+		else if(board[1][i] == O)
+		{
+			if(board[2][i] == O && board[0][i] == O)
+				oCur = 3;
+			else if(board[2][i] == O || board[0][i] == O)
+				oCur = 2;
+		}
+		if(xCur > xHigh)
+			xHigh = xCur;
+		if(oCur > oHigh)
+			oHigh = oCur;
+	}
+	for(int i=0; i<BOARD_ROWS; i++)
+	{
+		if(board[i][0] == X && board[i][1] == X)
+		{
+			if(board[i][2] == X)
+			{
+				if(board[i][3] == X)
+					xCur = 4;
+				else xCur = 3;
+			}
+			else xCur = 2;
+		}
+		else if(board[i][1] == X && board[i][2] == X)
+		{
+			if(board[i][3] == X)
+				xCur = 3;
+			else xCur = 2;
+		}
+		else if(board[i][2] == X && board[i][3] == X)
+			xCur = 2;
+		if(board[i][0] == O && board[i][1] == O)
+		{
+			if(board[i][2] == O)
+			{
+				if(board[i][3] == O)
+					oCur = 4;
+				else oCur = 3;
+			}
+			else oCur = 2;
+		}
+		else if(board[i][1] == O && board[i][2] == O)
+		{
+			if(board[i][3] == O)
+				oCur = 3;
+			else oCur = 2;
+		}
+		else if(board[i][2] == O && board[i][3] == O)
+			oCur = 2;
+		if(xCur > xHigh)
+			xHigh = xCur;
+		if(oCur > oHigh)
+			oHigh = oCur;
+	}
+	if(xHigh == oHigh)
+		return Blank;
+	if(xHigh > oHigh)
+		return X;
+	return O;
+};
--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -18,3 +18,246 @@ TEST(PiezasTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
 }
+
+TEST(PiezasTest, BoardIsClear)
+{
+	Piezas p;
+	ASSERT_EQ(p.pieceAt(0,0), Blank);
+}
+
+TEST(PiezasTest, OoBTopLeftPiece)
+{
+	Piezas p;
+	ASSERT_EQ(p.pieceAt(3,0), Invalid);
+}
+
+TEST(PiezasTest, OoBTopRightPiece)
+{
+	Piezas p;
+	ASSERT_EQ(p.pieceAt(3,4), Invalid);
+}
+
+TEST(PiezasTest, OoBBottomLeftPiece)
+{
+	Piezas p;
+	ASSERT_EQ(p.pieceAt(-1,0), Invalid);
+}
+
+TEST(PiezasTest, OoBBottomRightPiece)
+{
+	Piezas p;
+	ASSERT_EQ(p.pieceAt(0,4), Invalid);
+}
+
+TEST(PiezasTest, DropPiece)
+{
+	Piezas p;
+	p.dropPiece(0);
+	ASSERT_EQ(p.pieceAt(0,0), X);
+}
+
+TEST(PiezasTest, DropPieceSameCol)
+{
+	Piezas p;
+	p.dropPiece(0);
+	p.dropPiece(0);
+	ASSERT_EQ(p.pieceAt(1,0), O);
+}
+
+TEST(PiezasTest, DropPieceSameColx4)
+{
+	Piezas p;
+	p.dropPiece(0);
+	p.dropPiece(0);
+	p.dropPiece(0);
+	ASSERT_EQ(p.dropPiece(0), Blank);
+}
+
+TEST(PiezasTest, DropPieceOoBLeft)
+{
+	Piezas p;
+	ASSERT_EQ(p.dropPiece(-1), Invalid);
+}
+
+TEST(PiezasTest, DropPieceOobRight)
+{
+	Piezas p;
+	ASSERT_EQ(p.dropPiece(4), Invalid);
+}
+
+TEST(PiezasTest, BoardClears)
+{
+	Piezas p;
+	p.dropPiece(1);
+	p.reset();
+	ASSERT_EQ(p.pieceAt(0,1), Blank);
+}
+
+TEST(PiezasTest, FillBoard)
+{
+	Piezas p;
+	p.dropPiece(0);
+	p.dropPiece(1);
+	p.dropPiece(2);
+	ASSERT_EQ(p.dropPiece(3), O);
+}
+
+TEST(PiezasTest, GameStateBoardNotFilled)
+{
+	Piezas p;
+	ASSERT_EQ(p.gameState(), Invalid);
+}
+
+TEST(PiezasTest, XWinsByRow)
+{
+	Piezas p;
+	p.dropPiece(0);
+	p.dropPiece(0);
+	p.dropPiece(1);
+	p.dropPiece(1);
+	p.dropPiece(2);
+	p.dropPiece(2);
+	p.dropPiece(3);
+	p.dropPiece(1);
+	p.dropPiece(3);
+	p.dropPiece(3);
+	p.dropPiece(0);
+	p.dropPiece(2);
+	ASSERT_EQ(p.gameState(), X);
+}
+
+TEST(PiezasTest, TieGameRowandColE3)
+{
+	Piezas p;
+	p.dropPiece(0);
+	p.dropPiece(1);
+	p.dropPiece(0);
+	p.dropPiece(1);
+	p.dropPiece(0);
+	p.dropPiece(3);
+	p.dropPiece(2);
+	p.dropPiece(2);
+	p.dropPiece(1);
+	p.dropPiece(3);
+	p.dropPiece(3);
+	p.dropPiece(2);
+	ASSERT_EQ(p.gameState(), Blank);
+}
+
+TEST(PiezasTest, OWinsWith3)
+{
+	Piezas p;
+	p.dropPiece(0);
+	p.dropPiece(1);
+	p.dropPiece(0);
+	p.dropPiece(1);
+	p.dropPiece(1);
+	p.dropPiece(2);
+	p.dropPiece(2);
+	p.dropPiece(3);
+	p.dropPiece(3);
+	p.dropPiece(3);
+	p.dropPiece(2);
+	p.dropPiece(0);
+	ASSERT_EQ(p.gameState(), O);
+}
+
+TEST(PiezasTest, GameStateOnlyOneRowandColFilled)
+{
+	Piezas p;
+	p.dropPiece(0);
+	p.dropPiece(0);
+	p.dropPiece(0);
+	p.dropPiece(1);
+	p.dropPiece(2);
+	p.dropPiece(3);
+	ASSERT_EQ(p.gameState(), Invalid);
+}
+
+TEST(PiezasTest, GameStateLastSpotEmpty)
+{
+	Piezas p;
+	p.dropPiece(0);
+	p.dropPiece(0);
+	p.dropPiece(0);
+	p.dropPiece(1);
+	p.dropPiece(1);
+	p.dropPiece(1);
+	p.dropPiece(2);
+	p.dropPiece(2);
+	p.dropPiece(2);
+	p.dropPiece(3);
+	p.dropPiece(3);
+	ASSERT_EQ(p.gameState(), Invalid);
+}
+
+// The tests past this point are intended to get more code coverage
+TEST(PiezasTest, GameStateLastThreeO)
+{
+	Piezas p;
+	p.dropPiece(0);
+	p.dropPiece(1);
+	p.dropPiece(0);
+	p.dropPiece(2);
+	p.dropPiece(0);
+	p.dropPiece(3);
+	ASSERT_EQ(p.gameState(), Invalid);
+}
+
+TEST(PiezasTest, GameStateLastTwoO)
+{
+	Piezas p;
+	p.dropPiece(0);
+	p.dropPiece(3);
+	p.dropPiece(0);
+	p.dropPiece(2);
+	ASSERT_EQ(p.gameState(), Invalid);
+}
+
+TEST(PiezasTest, GameStateMiddleTwoO)
+{
+	Piezas p;
+	p.dropPiece(0);
+	p.dropPiece(1);
+	p.dropPiece(3);
+	p.dropPiece(2);
+	ASSERT_EQ(p.gameState(), Invalid);
+}
+
+TEST(PiezasTest, GameStateMoreCov)
+{
+	Piezas p;
+	p.dropPiece(0);
+	p.dropPiece(0);
+	p.dropPiece(1);
+	p.dropPiece(1);
+	p.dropPiece(2);
+	p.dropPiece(3);
+	p.dropPiece(3);
+	ASSERT_EQ(p.gameState(), Invalid);
+}
+
+TEST(PiezasTest, GameStateXConditions)
+{
+	Piezas p;
+	p.dropPiece(1);
+	p.dropPiece(0);
+	p.dropPiece(2);
+	p.dropPiece(1);
+	p.dropPiece(3);
+	p.dropPiece(0);
+	p.dropPiece(2);
+	p.dropPiece(0);
+	p.dropPiece(3);
+	ASSERT_EQ(p.gameState(), Invalid);
+}
+
+TEST(PiezasTest, GameStateXsMiddleofRow)
+{
+	Piezas p;
+	p.dropPiece(1);
+	p.dropPiece(0);
+	p.dropPiece(2);
+	ASSERT_EQ(p.gameState(), Invalid);
+}
+
